551b7955ac448c40e00146bed6c0e80e41702627
CarstenHensel <hensel.carsten@gmail.com>
2022-03-02 18:19:33 +0000

    accessing the particle info inside a jet

--- drivers/jet_extractor_driver.py
+++ drivers/jet_extractor_driver.py
@@ -2,84 +2,48 @@ from __future__ import absolute_import, unicode_literals
 from pyLCIO.drivers.Driver import Driver
 from ROOT import TH1D, TCanvas
 from sixlcio.moves import input
-
+from collections import OrderedDict
 
 
 class JetExtractorDriver( Driver ):
     def __init__( self ):
         ''' Constructor '''
         Driver.__init__( self )
-        self.histograms = {}
+        
     
     def startOfData( self ):
         ''' Method called by the event loop at the beginning of the loop '''
         
-        # Create two histograms for energy and pT
-        self.histograms['Energy'] = TH1D( 'Energy', 'Energy;Energy [GeV];Entries', 50, 0., 260. )
-        self.histograms['Pt'] = TH1D( 'Pt', 'pT;p_T [GeV];Entries', 50, 0., 100. )
-        self.histograms['PDGID'] = TH1D( 'PDGID', 'PDG ID;PDG ID;Entries', 1200, -600, 600. )
-        self.histograms['GeneratorStatus'] = TH1D( 'GeneratorStatus', 'Generator Status;Generator Status;Entries', 4, -0.5, 3.5 )
+        # Create ordered dictionary 
+        self.v = OrderedDict()
     
     def processEvent( self, event ):
         ''' Method called by the event loop for each event '''
-
-        print(event.getCollectionNames())
-        jet_collection = event.getCollection("Durham2Jets")
-        print(jet_collection)
-
-        for jet in jet_collection:
-            print(jet.getEnergy())
-
-        jet_collection = event.getCollection("Durham3Jets")
-        print(jet_collection)
-
-        for jet in jet_collection:
-            print(jet.getEnergy())
-
-
-        jet_collection = event.getCollection("Durham4Jets")
-        print(jet_collection)
-
-        for jet in jet_collection:
-            print(jet.getEnergy())
-
-
-        et_collection = event.getCollection("Durham5Jets")
-        print(jet_collection)
-
-        for jet in jet_collection:
-            print(jet.getEnergy())
 
 
         jet_collection = event.getCollection("Durham6Jets")
         print(jet_collection)
 
         for jet in jet_collection:
-            print(jet.getEnergy())
+            jet_lorentz_vec = jet.getLorentzVec()
+            jet_energy = jet_lorentz_vec.Energy()
+            jet_eta = jet_lorentz_vec.Eta()
+            jet_phi = jet_lorentz_vec.Phi()
+
+            # get particle from jet:
+            particles = jet.getParticles()
+            for particle in particles:
+                particle_lorentz_vec = particle.getLorentzVec()
+                particle_energy = particle_lorentz_vec.Energy()
+                particle_eta = particle_lorentz_vec.Eta()
+                particle_phi = particle_lorentz_vec.Phi()
+
         
-        # Get the MCParticle collection from the event
-        ##mcParticles = event.getMcParticles()
-        
-        # Loop over all MCParticles
-        ##for mcParticle in mcParticles:
-        ##    
-        ##    # Get the four vector of the MCParticle
-        ##    v = mcParticle.getLorentzVec()
-        ##    
-        ##    # Fill the histograms
-        ##    self.histograms['Energy'].Fill( v.Energy() )
-        ##    self.histograms['Pt'].Fill( v.Pt() )
-        ##    self.histograms['PDGID'].Fill( mcParticle.getPDG() )
-        ##    self.histograms['GeneratorStatus'].Fill( mcParticle.getGeneratorStatus() )
         
     def endOfData( self ):
         ''' Method called by the event loop at the end of the loop '''
         
         # Create a canvas for each histogram and draw them
-        plots = []
-        for histogramName in self.histograms:
-            plot = TCanvas( 'c%s' % ( histogramName ), histogramName )
-            self.histograms[histogramName].Draw()
-            plots.append( plot )
+        
         
         userInput = input('Press any key to continue')
