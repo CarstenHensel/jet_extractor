10fb828d69f41cc529dacf9e6a922301ae7922f0
CarstenHensel <hensel.carsten@gmail.com>
2022-03-04 15:52:59 +0000

    particle properties found

--- drivers/jet_extractor_driver.py
+++ drivers/jet_extractor_driver.py
@@ -3,42 +3,105 @@ from pyLCIO.drivers.Driver import Driver
 from ROOT import TH1D, TCanvas
 from sixlcio.moves import input
 from collections import OrderedDict
+import json
+from dataclasses import dataclass
+from math import log, sqrt
+
+@dataclass
+class ParticleContainer():
+    delta_eta  : float
+    delta_phi  : float
+    log_pt     : float
+    log_e      : float
+    log_rel_pt : float 
+    log_rel_e  : float 
+    delta_R    : float
+        
+    def get_dict(self):
+        d = {}
+        d["delta_eta"]  = self.delta_eta  
+        d["delta_phi"]  = self.delta_phi 
+        d["log_pt"]     = self.log_pt
+        d["log_e"]      = self.log_e
+        d["log_rel_pt"] = self.log_rel_pt
+        d["log_rel_e"]  = self.log_rel_e
+        d["delta_R"]    = self.delta_R
+        return d
+
+@dataclass
+class JetContainer():
+    eta    : float
+    phi    : float
+    energy : float
+    pt     : float
 
 
 class JetExtractorDriver( Driver ):
     def __init__( self ):
         ''' Constructor '''
         Driver.__init__( self )
-        
+
     
     def startOfData( self ):
         ''' Method called by the event loop at the beginning of the loop '''
         
         # Create ordered dictionary 
         self.v = OrderedDict()
+
+        p = ParticleContainer(1, 2, 3, 4, 5, 6, 7)
+       
+        with open('data.json', 'w') as f:
+            json.dump(p.get_dict(), f)
     
     def processEvent( self, event ):
         ''' Method called by the event loop for each event '''
+        n = 0
+        event_numer = event.getEventNumber()
 
+        out_put = {"jet content": {"ID":-1, "delta_eta":[], "delta_phi":[], "log pT":[], \
+                                  "log E":[], "log rel pT":[], "log rel E":[], "Delta R": -1}}
 
         jet_collection = event.getCollection("Durham6Jets")
-        print(jet_collection)
+        print(jet_collection, jet_collection.size())
+        
+        n_jet = 0
+        output_jets = []
+        for j in jet_collection:
+            n_jet += 1
+            
+        
 
-        for jet in jet_collection:
-            jet_lorentz_vec = jet.getLorentzVec()
-            jet_energy = jet_lorentz_vec.Energy()
-            jet_eta = jet_lorentz_vec.Eta()
-            jet_phi = jet_lorentz_vec.Phi()
+            lorentz_vec = j.getLorentzVec()
+            energy = lorentz_vec.Energy()
+            eta = lorentz_vec.Eta()
+            phi = lorentz_vec.Phi()
+            pt = lorentz_vec.Pt()
+            jet = JetContainer(eta, phi, energy, pt)
+
+            output_jets.append(jet)
 
             # get particle from jet:
-            particles = jet.getParticles()
-            for particle in particles:
-                particle_lorentz_vec = particle.getLorentzVec()
-                particle_energy = particle_lorentz_vec.Energy()
-                particle_eta = particle_lorentz_vec.Eta()
-                particle_phi = particle_lorentz_vec.Phi()
+            particles = j.getParticles()
+            n_particles = 0
 
-        
+            output_particles = []
+            for p in particles:
+                n_particles += 1
+                p_lorentz_vec = p.getLorentzVec()
+                particle_energy = p_lorentz_vec.Energy()
+                particle_eta = p_lorentz_vec.Eta()
+                particle_phi = p_lorentz_vec.Phi()
+                particle_pt = p_lorentz_vec.Pt()
+
+                delta_eta = eta - particle_eta
+                delta_phi = phi - particle_phi
+                log_pt = log(particle_pt)
+                log_e = log(particle_energy)
+                log_rel_pt = log(particle_pt / pt)
+                log_rel_e = log(particle_energy / energy)
+                delta_r = sqrt(delta_eta**2 + delta_phi**2)
+                particle = ParticleContainer(delta_eta, delta_phi, log_pt, log_e, log_rel_pt, log_rel_e, delta_r)
+                output_particles.append(particle)
         
     def endOfData( self ):
         ''' Method called by the event loop at the end of the loop '''
